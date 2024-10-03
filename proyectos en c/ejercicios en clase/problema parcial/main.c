5d793ccaedd34a873f6c0c219638a5791e97fd1c
THEBRYAMD <delarosabrian18@gmail.com>
2024-10-03 16:38:35 -0400

    el parcial esta en los ejercicios de clase

---
+++ proyectos en c/ejercicios en clase/problema parcial/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int lmin;
+    int lmax;
+    float cost;
+    float conb;
+
+    printf("\n\n ingrese los litros minimos que su vehiculo necesita cada 100Km: ");
+    scanf("%d", &lmin);
+    printf("\n ingrese los litros maximos que su vehiculo necesita cada 100km: ");
+    scanf("%d", &lmax);
+    printf("\n ingrese el precio del combustible por galon: ");
+    scanf("%f", &conb);
+printf("\n consumo L/100km | precio por km");
+    for( lmin; lmin <= lmax; lmin++)
+    {
+        cost = lmin / 3.78;
+        cost = cost * conb;
+        cost = cost / 100;
+        printf("\n| %dL            =       %5.2f pesos |", lmin, cost);
+        cost = 0.0;
+    }
+    return 0;
+}
